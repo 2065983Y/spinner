43a82d5663a997f4ec8b632efe828facd35f7f36
Ivaylo Valkov <2064491V@student.gla.ac.uk>
2015-10-04 09:43:35 +0100

    Levels up

--- levels.py
+++ levels.py
@@ -1,6 +1,42 @@
 
 
 level_1 = [
+    list("........."),
+    list("........."),
+    list("........."),
+    list("........."),
+    list("........."),
+    list("........."),
+    list("........."),
+    list("P.......G"),
+    list("SSSSSSSSS"),
+    ]
+
+level_2 = [
+    list("........."),
+    list("........."),
+    list("........."),
+    list("........."),
+    list("........."),
+    list("........G"),
+    list("........S"),
+    list("P.C....SS"),
+    list("SSSSSSSSS"),
+    ]
+
+level_3 = [
+    list("....SSSSS"),
+    list("....S...S"),
+    list("....SG..S"),
+    list("........S"),
+    list("........S"),
+    list("........S"),
+    list("........S"),
+    list("P.......S"),
+    list("SSSSSSSSS"),
+    ]
+
+level_4 = [
     list("........GSS"),
     list("S........SS"),
     list("D......C.SS"),
